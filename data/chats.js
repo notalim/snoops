9761759b4be9cf368fca30651ca72d8425e7963a
Yousaf-R878 <yousaf.rajput878@gmail.com>
2023-04-10 16:22:07 -0400

    Almost done with chat data funcs

--- data/chats.js
+++ data/chats.js
@@ -7,22 +7,53 @@ const chatCollection = await chats();
 
 const getAllChatsACenter = async (centerID) => {
     centerID = validation.checkId(centerID, "CenterID");
-    const centerChats = await chatCollection.find({centerID: new ObjectId(centerID)}).toArray();
+    const centerChats = await chatCollection.find({centerId: new ObjectId(centerID)}).toArray();
+    if(centerChats === null){
+        throw `No chats for aCenter with ID ${centerID}`
+    }
     return centerChats;
 }
 
 const getAllChatsUser = async (userID) => {
     userID = validation.checkId(userID, "UserID");
-    const userChats = await chatCollection.find({userID: new ObjectId(userID)}).toArray();
+    const userChats = await chatCollection.find({userId: new ObjectId(userID)}).toArray();
+    if(userChats === null){
+        throw `No chats for aCenter with ID ${userID}`
+    }
     return userChats;
 }
 
 const createChat = async (userID, centerID) => {
+    userID = validation.checkId(userID, "UserID");
+    centerID = validation.checkId(centerID, "CenterID");
 
+    const newChat = {
+        userId : userID,
+        centerId : centerID,
+        messages : []
+    }
+
+    const newInsertInformation = await chatCollection.insertOne(newChat);
+    if (newInsertInformation.insertedCount === 0) {
+        throw `Could not create chat between user: ${userID} and aCenter: ${centerID}`;
+    }
+
+    return newChat;
 }
 
 const getChat = async (userID, centerID) => {
+    userID = validation.checkId(userID, "UserID");
+    centerID = validation.checkId(centerID, "CenterID");
 
+    const userChats = await chatCollection.find(
+        {userId: new ObjectId(userID),
+        centerId: new ObjectId(centerID)});
+
+    if(userChats === null){
+        throw `No chat between user: ${userID} and aCenter: ${centerID}`;
+    }
+
+    return userChats;
 }
 
 const postMessage = async (
