d0a5ab1407bb53ac160311927072b6680f671d61
notalim <alimneedstowork@gmail.com>
2023-04-30 19:56:14 -0400

    started the updateForm modal + did the client side validaiton

---
+++ public/js/login-validation.js
@@ -0,0 +1,27 @@
+import clientValidation from "./client-validation-methods.js";
+
+let loginForm = document.getElementById("login-form");
+
+if (loginForm) {
+    let emailAddressIn = document.getElementById("email");
+    let passwordIn = document.getElementById("password");
+    let error = document.getElementById("error");
+
+    loginForm.addEventListener("submit", function (event) {
+        try {
+            error.hidden = true;
+            event.preventDefault();
+            emailAddressIn.value = clientValidation.checkEmailRegex(
+                emailAddressIn.value,
+                "Email Address"
+            );
+            passwordIn.value = clientValidation.checkString(passwordIn.value, "Password");
+            event.target.submit();
+            error.hidden = false;
+        } catch (e) {
+            event.preventDefault();
+            error.hidden = false;
+            error.innerHTML = e;
+        }
+    });
+}
