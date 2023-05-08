a6f84d1d7f51a2b741f547b3c1cbbb3a9e5a98ba
Yousaf-R878 <yousaf.rajput878@gmail.com>
2023-05-08 18:09:41 -0400

    validating

--- routes/main.js
+++ routes/main.js
@@ -9,18 +9,18 @@ router.get("/", async (req, res) => {
         } else if (acenterId) {
             return res.redirect("/ac-dashboard");
         }
-        return res.render("index");
+        return res.render("index", {title: "Snoops!"});
     } catch (error) {
         return res.status(500).json({ error: error.toString() });
     }
 });
 
 router.get("/resources", function (req, res) {
-    return res.render("resources");
+    return res.render("resources", {title: "Resources"});
 });
 
 router.get("/about", async (req, res) => {
-    return res.render("about");
+    return res.render("about", {title: "About Snoops"});
 });
 
 export default router;
