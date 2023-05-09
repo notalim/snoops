c0c68a03afa420cc90b817e75288a18e5528809a
Yousaf-R878 <yousaf.rajput878@gmail.com>
2023-05-09 16:01:19 -0400

    redirecting post req for chats

--- routes/chats.js
+++ routes/chats.js
@@ -220,7 +220,7 @@ router.route("/:uid/:acid").post(async (req, res) => {
     } catch (e) {
         try {
             const newChat = await chatData.createChat(uid, acid);
-            return res.status(200).json({ Chat: newChat });
+            return res.status(200).redirect("user-chats", { Chat: newChat });
         } catch (e) {
             return res.status(500).json({ error: e });
         }
